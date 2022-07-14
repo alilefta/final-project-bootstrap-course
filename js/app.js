1c09245bd44d59006093ee509b25b0d07741371b
Ali Lefta <alilefta95@gmail.com>
2022-07-14 15:17:13 +0300

    improvments

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-import "../node_modules/jquery/dist/jquery.slim.min.js";
-import "../node_modules/popper.js/dist/umd/popper.min.js";
-import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+// import "../node_modules/jquery/dist/jquery.slim.min.js";
+// import "../node_modules/popper.js/dist/umd/popper.min.js";
+// import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 $("document").ready(function () {
 	$(".our--work .card").hover(
-		function (e) {
+		function () {
 			$(this.querySelector(".card-img-top")).addClass("hover"); // $(this).addClass("hover");
 
 			$(this.querySelector(".hovered--text")).addClass("show");
@@ -13,13 +13,4 @@ $("document").ready(function () {
 			$(this.querySelector(".hovered--text")).removeClass("show");
 		}
 	);
-}); // document.addEventListener("DOMContentLoaded", function () {
-// 	document
-// 		.querySelector(".our--work .card")
-// 		.addEventListener("mouseenter", function (e) {
-// 			console.log(e.target);
-// 			e.target.querySelector(".card-img-top").classList.add("hover");
-// 		});
-// });
-
-//# sourceMappingURL=app.js.map
+});
